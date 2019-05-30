431fa900e527a08d38d03b5f6392b511e14bbb6c
kami <cordion_nap@yahoo.co.jp>
2019-05-31 01:56:15 +0900

    Test

--- app/src/main/java/comkamiapk/realm_todo/MainActivity.kt
+++ app/src/main/java/comkamiapk/realm_todo/MainActivity.kt
@@ -20,27 +20,31 @@ class MainActivity : AppCompatActivity() {
         //インスタンスの取得。
         realm = Realm.getDefaultInstance()
 
-
+        //保存する。
         save.setOnClickListener {
-            if(editText.getText().toString() != ""){
+
+            if(editText.getText().toString() != ""){ //editTextに文字が何か入力されているとき
+
                 //データベースへの追加
                 realm.executeTransaction { realm: Realm ->
                     val maxId = realm.where<TodoModel>().max("id")
                     val nextId = (maxId?.toLong() ?: 0L) + 1
-                    //データを新しく追加するためにオブジェクトを作る。
                     //引数はプライマリーキーがつけられているidを取る
                     //今回はnextIdがidに相当
-                    val db = realm.createObject<TodoModel>(nextId)
+                    val db = realm.createObject<TodoModel>(nextId) //データを新しく追加するためにオブジェクトを作る。
                     db.todo = editText.getText().toString()
                 }
+
                 editText.setText("")
                 Toast.makeText(this, "保存したよ。", Toast.LENGTH_LONG).show()
             }else {
                 Toast.makeText(this, "何かを記入してね!", Toast.LENGTH_LONG).show()
             }
+
+
         }
 
-
+        //アクティビティの遷移
         dis.setOnClickListener {
             val intent = Intent(this,TodoDisplay::class.java)
             startActivity(intent)
--- app/src/main/java/comkamiapk/realm_todo/TodoDisplay.kt
+++ app/src/main/java/comkamiapk/realm_todo/TodoDisplay.kt
@@ -20,21 +20,24 @@ class TodoDisplay : AppCompatActivity() {
 
 
         //id関連の変数の初期化
-        var id_min : Long  = -1
-        var id_max : Long = -2
-        var id_now : Long = 0
-        var id_delete : Long = -1
+        //TodoModelにデータがないなら、id_now > id_maxが成り立つように初期化する。
+        var id_min : Long  = -1 //idの最小値
+        var id_max : Long = -2 //idの最大値
+        var id_now : Long = 0 //表示するもののid
+        var id_delete : Long = -1 //消したいid
 
 
-        //クエリと初期化
+
         //これはidの値を取得しているが型がNumberというものなのでキャストが必要
         var Now = realm.where<TodoModel>().min("id")
         var Max = realm.where<TodoModel>().max("id")
-
+        //どのデータを取得するのかはidを用いる。
+        //id_nowの一個前のidを取得したい操作を考えるだけならリスト型配列を用いたほうがよい。
+        //ただTodoModelをリスト型配列で取得してもいいが、データの削除に対応したいので・・・
         var Todo = realm.where<TodoModel>().equalTo("id",Now?.toLong() ?: -1).findFirst()
 
 
-
+        //Todoがnullとなるときは、データが一つもない時。
         if(Todo != null){
             id_min = Todo.id
             //一番小さいidのものを一番最初に持ってくる
@@ -47,88 +50,76 @@ class TodoDisplay : AppCompatActivity() {
         todo_dis.setText((Todo?.todo ?:"何もやることはないよ！").toString()   + (Todo?.id ?:"").toString())
 
         //次のやることを表示
-
         next_todo.setOnClickListener {
 
             //最後までは表示するものが切り替わる。
             if(id_now < id_max) {
-                // Todoの更新
+                // 表示されているid_nowより一つ大きいidを取得
                 Now = realm.where<TodoModel>().greaterThan("id",id_now).min("id")
+                //id_nowの更新
                 id_now = Now?.toLong() ?: 0L
+                //Todoの更新
                 Todo = realm.where<TodoModel>().equalTo("id",id_now).findFirst()
                 todo_dis.setText((Todo?.todo ?:"何もやることはないよ！").toString()   + (Todo?.id ?:"").toString())
-            }else{
-                //id_now == id_max またはTodoがnullのとき
+            }else{ //id_max <= id_now
                 Toast.makeText(this, "やることを追加してね!", Toast.LENGTH_LONG).show()
             }
         }
-
-        //ここはok
+        //一つ前のやることを表示
         back.setOnClickListener {
             if(id_min < id_now){
+                //lessThanを用いてfindFisrstなどでidを取得すると、一個前のidではなくid_minを取得してしまう
+                //そのため、現在のid_nowより小さいものを対象にして(lessThan)、その中から最大のidを取得すれば
+                //一つ前のidが取得できる
                 Now = realm.where<TodoModel>().lessThan("id",id_now).max("id")
                 id_now = Now?.toLong() ?: 0L
                 Todo = realm.where<TodoModel>().equalTo("id",id_now).findFirst()
                 todo_dis.setText((Todo?.todo ?:"何もやることはないよ！").toString()   + (Todo?.id ?:"").toString())
             }else{
+                //id_nowがid_minと同じ場合、トースト表示
                 Toast.makeText(this, "これを最初にやってしまおう!", Toast.LENGTH_LONG).show()
             }
         }
 
 
         //データの削除
-
-
-
-        /*流れ
-
-        ・id_nowの値を保持しておく。
-        ・id_nowの値を次に表示するものに変える
-            ・id_now == id_max　だったらid_maxの値を更新する
-            ・id_now == id_min だったらid_minの値を更新する
-
-
-        ・表示されているデータを消す。
-
-        ・更新されたid_nowの値に応じてテキストの更新をする。
-
-
-
-         */
+        //基本的には表示されているデータを消して、一つ前のやることを表示する。
         delete.setOnClickListener {
-
-            if(Todo != null){
+            if(Todo != null){ //データがある時、idの更新をする。
                 //削除用のid保持
                 id_delete = id_now
 
-                //表示用のid更新など
-                //データが一つしかないとき
-                if(id_min == id_max){
+                if(id_min == id_max){ //データが一つしかないとき
                     //next内の処理に合わせる
                     id_min = -1
                     id_max = -100
-                    Todo = null
-                } else if(id_now == id_max){
+                    Todo = null //ここでTodoをnullにする。
+                } else if(id_now == id_max){ //この場合、id_maxの値も更新する必要がある。
 
                     Now = realm.where<TodoModel>().lessThan("id",id_now).max("id")
                     id_max = Now?.toLong() ?: 0L
                     id_now = id_max
 
-                }else if(id_now == id_min){
+                }else if(id_now == id_min){ //
 
                     Now = realm.where<TodoModel>().greaterThan("id",id_now).min("id")
                     id_min = Now?.toLong() ?: 0L
                     id_now = id_min
 
-                }else{
+                }else{ //id_min < id_now < id_max　大半はここで処理がされる。
 
                     Now = realm.where<TodoModel>().lessThan("id",id_now).max("id")
                     id_now = Now?.toLong() ?: 0L
 
                 }
 
+                //id更新が終わった後、トースト表示
+                Toast.makeText(this, "お疲れさま!", Toast.LENGTH_LONG).show()
+
+                //id_max == -100となるときは、データが一つしかない場合。
+                //ここでテキストの更新をする。
                 if(id_max != -100L){
-                    Toast.makeText(this, "お疲れさま!", Toast.LENGTH_LONG).show()
+
                     Todo = realm.where<TodoModel>().equalTo("id",id_now).findFirst()
                     todo_dis.setText((Todo?.todo ?:"何もやることはないよ！").toString()   + (Todo?.id ?:"").toString())
                 }else{
@@ -136,11 +127,9 @@ class TodoDisplay : AppCompatActivity() {
                 }
 
 
-                //データ削除
+                //最後にデータを削除する
                 realm.executeTransaction { db: Realm ->
-                    db.where<TodoModel>().equalTo("id",id_delete)
-                        ?.findFirst()
-                        ?.deleteFromRealm()
+                    db.where<TodoModel>().equalTo("id",id_delete).findFirst()?.deleteFromRealm()
                 }
 
 
